bd5699112e49ecd90c887dde05df0560792a4b74
pdhung3012 <pdhung3012@gmail.com>
2018-11-22 22:57:05 -0600

    update web

--- src/net/viralpatel/struts2/TranslateServlet.java
+++ src/net/viralpatel/struts2/TranslateServlet.java
@@ -31,9 +31,6 @@ public class TranslateServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public TranslateServlet() {
         super();
         // TODO Auto-generated constructor stub
