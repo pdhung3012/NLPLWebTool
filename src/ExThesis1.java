b0f8b763d87d5afa7bd450ea81eb70da45bfdce1
hungphd <hungphd@iastate.edu>
2018-12-20 11:59:32 -0600

    update thesis

---
+++ src/ExThesis1.java
@@ -0,0 +1,8 @@
+
+public class ExThesis1 {
+	public static void main(String[] args) {
+//		initialize x and set its value to zero
+		int x=0;		
+	}
+}
+
---
+++ src/ExThesis2.java
@@ -0,0 +1,10 @@
+
+public class ExThesis2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// copy the file "a.txt" to "/temp/"
+		
+	}
+
+}
